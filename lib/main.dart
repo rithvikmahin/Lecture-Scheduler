a47218fe42e31ffd747a595bf6cb255f8bcf2a0e
Rithvik <rithvik7@illinois.edu>
2020-08-08 15:48:21 -0400

    Added course buttons and refactored the UI.
    Co-authored-by: sreyasa2 sreyasa2@illinois.edu
    Co-authored-by: tejalb2 tejalb2@illinois.edu

--- lib/main.dart
+++ lib/main.dart
@@ -15,22 +15,50 @@ class MyApp extends StatelessWidget {
             imageSection,
             courseSelection
           ],
+
         )
       )
       );
   }
 
   final Widget imageSection = Container(
-    child: Image.asset('images/logo.jpg')
+    child: Image.asset(
+      'images/logo.jpg',
+      height: 350,
+      width: 400,
+      )
   );
 
-  final Widget courseSelection = Container(
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.center, 
-      children: [
-        Text('PLEASE SELECT YOUR CLASS')
-      ],
-    )
+  final Widget courseSelection = Column (
+    children: [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center, 
+        children: [
+          Text(
+            'Please select your class',
+            style: TextStyle(fontFamily: 'Montserrat'))
+        ]
+      ),
+
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center, 
+        children: [
+          OutlineButton (
+            onPressed: () {},
+            borderSide: BorderSide (
+            color: Color(0xFF0C0C72), //Color of the border
+            style: BorderStyle.solid, //Style of the border
+            width: 1.2, //width of the border
+            ),
+            color: Colors.white,
+            child: Container (
+              child: const Text (
+                'CS 125', 
+                style: TextStyle(color: Color(0xFF0C0C72), fontFamily: 'Montserrat', fontWeight: FontWeight.bold)),
+            )
+          )
+        ]
+      ) 
+    ]
   );
 }
-
