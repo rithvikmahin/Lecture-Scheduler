2114126885a9a289f8ca24ba81a7c881353ed4d6
Rithvik <rithvik7@illinois.edu>
2020-08-09 14:46:44 -0400

    Abstracted classes, improved the UI.
    Co-authored-by: Sreyas <sreyasa2@illinois.edu>
    Co-authored-by: Tejal <tejalb2@illinois.edu>

--- lib/main.dart
+++ lib/main.dart
@@ -11,49 +11,63 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
             backgroundColor: Colors.white,
             body: Column(
-              children: [Center(child: imageSection), courseSelection],
+              // Sets the image and course selection in a column.
+              children: [
+                SizedBox(height: 60),
+                Center(child: imageSection), 
+                courseSelection
+                ],
             )));
   }
 
   final Widget imageSection = Container(
       child: Image.asset(
-    'images/logo.jpg',
-    height: 350,
+    'images/logo2.jpg',
+    height: 250,
     width: 400,
   ));
 
   final Widget courseSelection = Column(children: [
-    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-      Text('P L E A S E  S E L E C T  Y O U R  C L A S S',
-          style: TextStyle(fontFamily: 'Montserrat', fontSize: 10))
+    Row(
+      mainAxisAlignment: MainAxisAlignment.center, 
+      children: [
+      Text('P L E A S E  S E L E C T  Y O U R  C L A S S', 
+          style: TextStyle(fontFamily: 'Montserrat', fontSize: 17))
     ]),
+    SizedBox(height: 24),
     ClassButton("CS 125"),
-    ClassButton("Math 241")
+    SizedBox(height: 16),
+    ClassButton("MATH 241")
   ]);
 }
-
+ /* 
+  Makes specific button according to text.
+  */
 class ClassButton extends StatelessWidget {
-  String className;
-  ClassButton(String className) {
+  String className; // Variable for class name.
+  ClassButton(String className) { // Constructor for text.
     this.className = className;
   }
+  
   @override
   Widget build(BuildContext context) {
     return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-      OutlineButton(
-          onPressed: () {},
+      OutlineButton( // Using outline buttons
+          onPressed: () {
+
+          },
           borderSide: BorderSide(
-            color: Color(0xFF0C0C72), //Color of the border
-            style: BorderStyle.solid, //Style of the border
-            width: 1.2, //width of the border
+            color: Color(0xFF0C0C72), // Color of the border.
+            style: BorderStyle.solid, // Style of the border.
+            width: 1.2, // Width of the border.
           ),
           color: Colors.white,
           child: Container(
-            child: const Text('CS 125',
+            child: Text(className,
                 style: TextStyle(
-                    color: Color(0xFF0C0C72),
-                    fontFamily: 'Montserrat',
-                    fontWeight: FontWeight.bold)),
+                    color: Color(0xFF0C0C72), // Color of the Text.
+                    fontFamily: 'Montserrat', // Font of the Text.
+                    fontWeight: FontWeight.bold)) // Style of Font.
           ))
     ]);
   }
